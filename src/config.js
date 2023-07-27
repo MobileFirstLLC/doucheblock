f1a4a657b8a287202a4f7208911b4a33c7f3bd5f
mateohhh <mateohh@gmail.com>
2023-07-26 15:20:20 -1000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -43,7 +43,7 @@ export const defaultConfig = {
      * @constant
      * @type {string}
      */
-    allowWords: '',
+    allowWords: 'magazine',
     /**
      * mute instead of block
      * @constant
